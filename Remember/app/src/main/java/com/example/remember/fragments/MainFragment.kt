08e17f61ab74c16f9143b7644c4cbfa09b0c4bad
theBigUPs <114905556+theBigUPs@users.noreply.github.com>
2023-08-08 21:26:03 +0300

    preliminary work done

---
+++ Remember/app/src/main/java/com/example/remember/fragments/MainFragment.kt
@@ -0,0 +1,38 @@
+package com.example.remember.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.remember.R
+import com.example.remember.databinding.FragmentMainBinding
+import com.example.remember.models.MainViewModel
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        binding.testTv.text = viewModel.test()
+
+
+
+    }
+}
---
+++ Remember/app/src/main/java/com/example/remember/models/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.remember.models
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    fun test(): String {
+
+        return "ert"
+
+    }
+
+
+}
