34de1fd14e94a302b684791551ce757c8c09b745
theBigUPs <114905556+theBigUPs@users.noreply.github.com>
2023-08-28 00:44:20 +0300

    finished might add ui adjustments

--- VolumeAdjuster/app/src/main/java/com/example/volumeadjuster/ForegroundService.kt
+++ VolumeAdjuster/app/src/main/java/com/example/volumeadjuster/ForegroundService.kt
@@ -6,11 +6,16 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
 import android.os.Build
 import android.os.IBinder
 import android.widget.RemoteViews
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 
 class ForegroundService : Service() {
@@ -20,6 +25,7 @@ class ForegroundService : Service() {
         const val CHANNEL_ID = "MyForegroundServiceChannel"
         const val VOLUME_UP_ACTION = "VOLUME_UP_ACTION"
         const val VOLUME_DOWN_ACTION = "VOLUME_DOWN_ACTION"
+        const val VOLUME_MAX_ACTION = "VOLUME_MAX_ACTION"
         const val STOP_ACTION = "STOP_ACTION"
     }
 
@@ -33,13 +39,27 @@ class ForegroundService : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         if (intent != null) {
             when (intent.action) {
-                VOLUME_UP_ACTION -> {
-                    // Handle pause action
-                    // Example: Pause the ongoing task
+                VOLUME_MAX_ACTION->{
+
+                    val audioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
+                    val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, 0)
                 }
+                VOLUME_UP_ACTION -> {
+
+                    val audioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
+                    audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI)
+                }
+                VOLUME_DOWN_ACTION -> {
+
+                    val audioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
+                    audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI)
+                }
+
                 STOP_ACTION -> {
-                    // Handle stop action
-                    // Example: Stop the foreground service and clean up
+
+
+
                     stopForeground(Service.STOP_FOREGROUND_REMOVE)
                     stopSelf()
                 }
@@ -54,7 +74,7 @@ class ForegroundService : Service() {
             val channel = NotificationChannel(
                 CHANNEL_ID,
                 "Foreground Service Channel",
-                NotificationManager.IMPORTANCE_DEFAULT
+                NotificationManager.IMPORTANCE_LOW
             )
             val manager = getSystemService(NotificationManager::class.java)
             manager.createNotificationChannel(channel)
@@ -69,8 +89,30 @@ class ForegroundService : Service() {
         val stopPendingIntent = PendingIntent.getService(this, 0, stopIntent,
             PendingIntent.FLAG_IMMUTABLE)
 
-        //customLayout.setOnClickPendingIntent(R.id.pause_button, pausePendingIntent)
-        //customLayout.setOnClickPendingIntent(R.id.cancel, stopPendingIntent)
+
+        val volumeUpIntent = Intent(this, ForegroundService::class.java)
+            .setAction(VOLUME_UP_ACTION)
+        val volumeUpPendingIntent = PendingIntent.getService(this, 0, volumeUpIntent,
+            PendingIntent.FLAG_IMMUTABLE)
+
+
+        val volumeDownIntent = Intent(this, ForegroundService::class.java)
+            .setAction(VOLUME_DOWN_ACTION)
+        val volumeDownPendingIntent = PendingIntent.getService(this, 0, volumeDownIntent,
+            PendingIntent.FLAG_IMMUTABLE)
+
+
+
+        val volumeMaxIntent = Intent(this, ForegroundService::class.java)
+            .setAction(VOLUME_MAX_ACTION)
+        val volumeMaxPendingIntent = PendingIntent.getService(this, 0, volumeMaxIntent,
+            PendingIntent.FLAG_IMMUTABLE)
+
+
+        customLayout.setOnClickPendingIntent(R.id.cancel, stopPendingIntent)
+        customLayout.setOnClickPendingIntent(R.id.vol_up, volumeUpPendingIntent)
+        customLayout.setOnClickPendingIntent(R.id.vol_down, volumeDownPendingIntent)
+        customLayout.setOnClickPendingIntent(R.id.vol_max, volumeMaxPendingIntent)
 
         val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setCustomContentView(customLayout)
--- VolumeAdjuster/app/src/main/java/com/example/volumeadjuster/MainActivity.kt
+++ VolumeAdjuster/app/src/main/java/com/example/volumeadjuster/MainActivity.kt
@@ -1,17 +1,40 @@
 package com.example.volumeadjuster
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 
         val serviceIntent = Intent(this, ForegroundService::class.java)
         startService(serviceIntent)
 
 
+        //val filter = IntentFilter("close_app_action")
+        //LocalBroadcastManager.getInstance(this).registerReceiver(closeAppReceiver, filter)
+        finish()
     }
+
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        //LocalBroadcastManager.getInstance(this).unregisterReceiver(closeAppReceiver)
+    }
+
+
+
+
 }
